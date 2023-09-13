ceef9fb958c10d1f707759ef4ff995c833f36024
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-09-13 16:35:47 +0300

    fix: some email have not been tested

--- utils/regexp.js
+++ utils/regexp.js
@@ -1,4 +1,4 @@
-const regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-].[a-zA-Z0-9-.]+$/;
+const regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,}$/;
 const regexUrl = /^https?:\/\/[www.]?[-a-zA-Z0-9+&@/?=~_!:,.;()*'$\][]+\.[-a-zA-Z0-9+&@/?=~_!:,.;()*'$\][]+#?/;
 const regexInt16 = /[a-f0-9]*/;
 
